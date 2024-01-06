cada6ed27f38315a1305697236e9c65a00d63147
Akhilesh Prasanna <37016273+bruchimp3@users.noreply.github.com>
2024-01-07 02:26:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("I like Pizza");
+console.log("Its really good");
+//window.alert("I REALLY LOVE PIZZA");
+
+//This is a single line comment
+
+/* This is a multi line 
+comment*/
+
+//2. VARIABLES
+let firstName = "Akhilesh"
+let age = 26;
+let student = true;
+
+console.log("Hey",firstName);
+console.log("You are",age,"years old");
+console.log("Enrolled:",student);
+
+document.getElementById("p1").innerHTML = "Hello "+firstName;
+document.getElementById("p2").innerHTML = "You are "+age+" years old";
+document.getElementById("p3").innerHTML = "Enrolled: "+student;
